36acc74235690fc548ada3bd1cdd850263851b02
romain plantey <plantey.romain@gmail.com>
2021-11-16 14:39:57 +0100

    task manager ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
+import "@fortawesome/fontawesome-free/css/all"
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
